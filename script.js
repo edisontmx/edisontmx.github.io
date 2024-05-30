8e85e97a5946049acf1f85fcc0983364c22c49a4
edisontmx <edison.mc@gmail.com>
2024-05-29 21:26:48 -0500

    agrego cv pdf

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function mostrarOcultarMenu(){
 function seleccionar(){
     //se oculta el menu una ves que selecciona una opcion
     document.getElementById("nav").classList="";
-    manuVisible=false;
+    menuVisible=false;
 }
 //animaciones de las habilidades
 function efecto(){
@@ -21,8 +21,8 @@ function efecto(){
     if(long>=300){
         let habilidades=document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("HTML");
-        habilidades[2].classList.add("PHP");
+        habilidades[1].classList.add("Go");
+        habilidades[2].classList.add("Go");
         habilidades[3].classList.add("Python");
         habilidades[4].classList.add("Go");
         habilidades[5].classList.add("equipo");
@@ -30,6 +30,10 @@ function efecto(){
         habilidades[7].classList.add("comunicacion");
         habilidades[8].classList.add("PM");
         habilidades[9].classList.add("dedicacion");
+        habilidades[10].classList.add("dedicacion");
+        habilidades[11].classList.add("dedicacion");
+        habilidades[12].classList.add("dedicacion");
+        habilidades[13].classList.add("dedicacion");
     }
 }
 //deteccion del scrolling para aplcar la animacion de la barra
