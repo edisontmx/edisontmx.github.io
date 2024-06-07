3085e357fbb7037fae656be7d1176868d13424d0
edisontmx <edison.mc@gmail.com>
2024-06-06 19:23:01 -0500

    actualizacion 060624, corrijo animaciones de skills

--- script.js
+++ script.js
@@ -21,22 +21,22 @@ function efecto(){
     if(long>=300){
         let habilidades=document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("Go");
-        habilidades[2].classList.add("Go");
+        habilidades[1].classList.add("nodejs");
+        habilidades[2].classList.add("golang");
         habilidades[3].classList.add("Python");
-        habilidades[4].classList.add("Go");
-        habilidades[5].classList.add("equipo");
-        habilidades[6].classList.add("creatividad");
-        habilidades[7].classList.add("comunicacion");
-        habilidades[8].classList.add("PM");
-        habilidades[9].classList.add("dedicacion");
-        habilidades[10].classList.add("dedicacion");
-        habilidades[11].classList.add("dedicacion");
-        habilidades[12].classList.add("dedicacion");
-        habilidades[13].classList.add("dedicacion");
+        habilidades[4].classList.add("react");
+        habilidades[5].classList.add("mysql");
+        habilidades[6].classList.add("mongo");
+        habilidades[7].classList.add("github");
+        habilidades[8].classList.add("docker");
+        habilidades[9].classList.add("equipo");
+        habilidades[10].classList.add("activo");
+        habilidades[11].classList.add("auto");
+        habilidades[12].classList.add("result");
+        habilidades[13].classList.add("dedicado");
     }
 }
-//deteccion del scrolling para aplcar la animacion de la barra
+//deteccion del scrolling para aplicar la animacion de la barra
 window.onscroll=function(){
     efecto();
 }
